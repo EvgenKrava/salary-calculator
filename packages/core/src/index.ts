b118bf88da4369739bca61b1ed9670d54c158968
yevhenii <oldfromkb@gmail.com>
2026-08-03 13:42:58 +0300

    Export core public API

---
+++ packages/core/src/index.ts
@@ -0,0 +1,4 @@
+export * from './types';
+export { round2 } from './money';
+export { payPeriodsForMonth, isWithinPeriod } from './payPeriod';
+export { calculateSalaries } from './calculateSalaries';
